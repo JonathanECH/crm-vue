5f3f8027fe65e254c70889b6e66316e830aed9e7
JonathanECH <jonathanecampos3@gmail.com>
2026-09-13 12:40:44 -0400

    Se agregó Tailwind CSS, se creó una nueva vista para agregar un nuevo cliente y se creó un microcomponente para manejar las rutas entre las páginas.

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,12 @@ import { RouterView } from 'vue-router';
 </script>
 
 <template>
-  <div></div>
-  <RouterView />
+  <header class="bg-slate-800">
+    <div class="mx-auto max-w-6xl py-10">
+      <h1 class="text-4xl font-extrabold text-white">Administrado de Clientes</h1>
+    </div>
+  </header>
+  <main class="mx-auto max-w-6xl mt-10">
+      <RouterView />
+  </main>
 </template>
-
---
+++ src/components/UI/RouterLink.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+defineProps({
+  to: {
+    type: String
+  },
+})
+</script>
+<template>
+  <div>
+    <RouterLink :to="{ name: to }" class="block md:inline-block rounded-xl bg-blue-600 text-white
+    uppercase font-bold text-sm shadow hover:bg-blue-500 py-2 px-3">
+      <slot></slot>
+    </RouterLink>
+  </div>
+</template>
