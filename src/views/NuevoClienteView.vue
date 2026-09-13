a9703625337a0702bef55aa40fb3e31c53bf3fb6
JonathanECH <jonathanecampos3@gmail.com>
2026-09-13 14:09:17 -0400

    Se creó un formulario usando una libreria para esto que lo que hace es que valida los imput por nosotros.

--- src/views/NuevoClienteView.vue
+++ src/views/NuevoClienteView.vue
@@ -1,7 +1,7 @@
 <script setup>
 import RouterLink from '@/components/UI/RouterLink.vue';
 import Heading from '@/components/UI/HeadingVue.vue';
-
+import { FormKit } from '@formkit/vue';
 defineProps({
   titulo: {
     type: String
@@ -15,5 +15,22 @@ defineProps({
       <RouterLink to="inicio">Volver</RouterLink>
     </div>
     <Heading>{{ titulo }}</Heading>
+    <div class="mx-auto mt-10 bg-white shadow">
+      <div class="mx-auto md:w-2/3 py-20 px-6">
+        <FormKit type="form">
+          <FormKit type="text" label="Nombre" prefix-icon="add" help="Escriba el nombre del cliente que desea resistrar"
+            validation="required" :validation-messages="{
+              required: 'El nombre del cliente es Obligatorio',
+            }" />
+        </FormKit>
+      </div>
+
+    </div>
   </div>
 </template>
+
+<style>
+.formkit-wrapper {
+  max-width: 100%;
+}
+</style>
